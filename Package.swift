6a2f1100cd80c42676d1daca8175b48557d46f5a
Jon Mitts <mitts.3@gmail.com>
2023-09-28 12:32:35 -0400

    Updated iOS version.

--- Package.swift
+++ Package.swift
@@ -1,12 +1,14 @@
 // swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+//  Changed this, 2023-9-28, JM: .iOS("8.3"), .tvOS("9.0")  -->   .iOS(.v14), .tvOS(.v14)
+//  Followed pattern from: https://github.com/littleigloo/SVProgressHUD/compare/master...kiroskirin:SVProgressHUD:master 
 
 import PackageDescription
 
 let package = Package(
     name: "SVProgressHUD",
     platforms: [
-        .iOS("8.3"), .tvOS("9.0")
+        .iOS(.v14), .tvOS(.v14)
     ],
     products: [
         .library(
